ba448df0131b08f4b71e42b2e5e747c9d8d7d1f1
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-26 15:50:49 -0300

    user helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
+
   def show
-    @your_page = current_user == @user
+    @user_posts = @user.posts.order(created_at: :desc)
   end
 
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def your_page?(user)
+    current_user == user
+  end
+
+  def profile_title(user)
+    your_page?(user) ? "Your" : "#{user.username}'s"
+  end
 end
