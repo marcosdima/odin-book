f2e15fb8d702a732c787f5857e0579e7dfe5f27a
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-29 16:56:05 -0300

    like scopes

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,8 @@ class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true
 
   validates :user_id, uniqueness: { scope: %i[ likeable_id likeable_type ] }
+
+  scope :by_user, ->(user) { where(user: user) }
+  scope :for_likeable, ->(likeable) { where(likeable: likeable) }
+  scope :liked_by, ->(user, likeable) { by_user(user).for_likeable(likeable) }
 end
