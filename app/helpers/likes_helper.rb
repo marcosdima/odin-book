9427407af147d471329928da7ce35c1995facb88
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-06-29 16:56:38 -0300

    sub-views rework - comments and likes separated from post views

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,14 @@
 module LikesHelper
+  def like_params_for(likeable)
+    {
+      like: {
+        likeable_id: likeable.id,
+        likeable_type: likeable.class.name
+      }
+    }
+  end
+
+  def already_liked?(likeable)
+    Like.liked_by(current_user, likeable).any?
+  end
 end
