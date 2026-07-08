5779f46369a3ce8bf09419cc81b59b949d25b680
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-07-08 14:50:31 -0300

    user seed with gravatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,16 @@
 if Rails.env.development?
   User.find_or_create_by!(email: "test@gmail.com") do |user|
-    user.username = "test"
+    user.username = "test1"
     user.password = "123123"
   end
 
   User.find_or_create_by!(email: "test2@gmail.com") do |user|
     user.username = "test2"
+    user.password = "123123"
+  end
+
+  User.find_or_create_by!(email: "marcosdimatteo.dev@gmail.com") do |user|
+    user.username = "marcosdima"
     user.password = "123123"
   end
 end
